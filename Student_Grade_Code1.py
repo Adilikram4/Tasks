fc9e935c781efbfb7ed4c005ced22e5ed6f9546f
Adil Ikram <155573316+Adilikram4@users.noreply.github.com>
2024-01-11 00:47:33 +0500

    Add files via upload

---
+++ Student_Grade_Code1.py
@@ -0,0 +1,14 @@
+while True:
+    a = int(input("Enter your number between 0 to 100: "))
+    if a >= 90 :
+        print("your grade is A")
+    elif a >=80 :
+     print("your grade is B")
+    elif a >=70 :
+     print("your grade is C")
+    elif a >=60 :
+     print("your grade is D")
+    elif a >=0 :
+     print("your grade is F")
+else:
+    print("Enter right value :")
