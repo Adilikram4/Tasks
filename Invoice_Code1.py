844919463e89b6747e89de655f38f3dde217849f
Adil Ikram <155573316+Adilikram4@users.noreply.github.com>
2024-01-11 11:00:57 +0500

    Update Invoice_Code2.py

--- Invoice_Code1.py
+++ Invoice_Code1.py
@@ -1,12 +1,13 @@
 print("Welcome")
 while True:
-    amount = int(input("Enter your amount: "))
-    price = int(input("Enter your purchasing price: "))
+    amount = int(input("Enter your amount $: "))
+    price = int(input("Enter price of thing $: "))
     price1 = price*3
     gst = price1/100
     print("3% Tax apply : ", gst)
     a = int(price+gst)
-    print ("Your remaining Amount: " , amount - a)
+    print ("Your Gross Total: ",a,'$')
+    print ("Your remaining Amount: ",amount - a,'$')
     next_calculation = input("continue shopping? (yes/no): ")
     if next_calculation == "no":
         print("Thank you for Shopping")
