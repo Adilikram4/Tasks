fefd668e7a8bb931e4faf9371213fbb3eee23555
Adil Ikram <155573316+Adilikram4@users.noreply.github.com>
2024-01-11 15:12:31 +0500

    Update Student_Grade_Code1.py

--- Student_Grade_Code1.py
+++ Student_Grade_Code1.py
@@ -1,15 +1,15 @@
 print("Hello student")
 while True:
-    marks = float(input("Enter your number between 0-100:\n"))
+    marks = float(input("Enter your number between 0-100:   "))
     if 90 <= marks <= 100 :
-        grade = ("A")
+        print("Your Grade is: A\n")
     elif 80 <= marks <= 89 :
-        grade = ("B")
+        print("Your Grade is: B\n")
     elif 70 <= marks <= 79 :
-        grade = ("C")
+        print("Your Grade is: C\n")
     elif 60 <= marks <= 69 :
-        grade = ("D")
+        print("Your Grade is: D\n")
+    elif 00 <= marks <= 59 :
+        print("Your Grade is: F\n")
     else:
-        00 <= marks <= 59
-        grade = ("F")
-    print("Your Grade is:", grade,"\n")
+        print("Invalid Value\n     Enter Again\n")
